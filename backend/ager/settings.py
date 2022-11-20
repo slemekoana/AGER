8ddd8b8180701fbe04d980d0913c080465124764
Javi <javiecija96@gmail.com>
2022-11-20 22:04:35 +0100

    readapt to django

--- backend/ager/settings.py
+++ backend/ager/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parents[2]
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = '*9hy_y6!gjtl+mh8va#2($8k^3c+8l0c#aqkef=5$aiymwvxs$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
